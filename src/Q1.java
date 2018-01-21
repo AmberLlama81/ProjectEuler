54cbd9d6dfd6e0f01da14c19350e965c1bfc88c0
AmberLlama81 <amberllama811@gmail.com>
2018-01-21 20:54:01 +0900

    First attempt for Project Euler Q1.

---
+++ src/Q1.java
@@ -0,0 +1,20 @@
+public class Q1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                sum += i;
+                System.out.println(i);
+            }
+            if (i % 5 == 0 && i % 3 != 0) {
+                sum += i;
+                System.out.println(i);
+            }
+            if (i % 5 == 0 && i % 3 == 0) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
